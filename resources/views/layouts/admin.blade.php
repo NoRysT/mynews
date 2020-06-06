a17a8a26816479a7880e84c1de121cd831ee23d0
Natori <dolphina57@gmail.com>
2020-06-06 06:17:14 +0000

    10

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Csrf Token ? -->
+        {{-- あとで --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        {{-- 各ページごとにタイトルタグを入れるため@yieldで開けておく？ --}}
+        <title>@yield('title')</title>
+        
+        <!-- Scripts -->
+        {{-- Laravel標準で用意されているJavaすクリプトを読込みます --}}
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+        
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" ref="stylesheet" type="text/css">
+        
+        <!--Styles -->
+        {{-- Laravel標準css読込み  --}}
+        <link href="{{ secure_asset('css/app.css') }}" rel="sylesheet">
+        {{-- あとで作るcss読込み --}}
+        <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            {{-- 画面上部に表示するナビゲーションバーです。 --}}
+            <nav class="navbar navbar-expan-md navbar-dark navbar-laravel">
+                <div calss="container">
+                    <a calss="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedConten" aria-controls="navbarSupportedConten" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    
+                    <div class="cllapse navbar-cllapse" id="navbarSupportedConten">
+                        <!-- Left side of Navbar -->
+                        <ul calss="navbar-nav mr-auto">
+                            
+                        </ul>
+                        
+                        <!-- Right side of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            {{-- 以下を追記 --}}
+                        <!-- Authentication Links -->
+                        {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
+                        @guest
+                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                        {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
+                            {{-- 以上までを追記 --}}
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            {{-- ここまでNavbar --}}
+            
+            <main calss="py-4">
+                {{-- コンテンツをここに入れるため@yield空け　--}}
+                @yeild('content')
+            </main>
+        </div>
+    </body>
+</html>
